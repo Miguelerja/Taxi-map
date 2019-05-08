91f2550d0ce1187f66b3ad4f2b0b33673d705c35
Miguelerja <miguelerja@gmail.com>
2019-05-08 08:54:19 +0200

    Add render tests to App

--- frontend/src/tests/components/App.test.js
+++ frontend/src/tests/components/App.test.js
@@ -1,3 +1,31 @@
 import React from 'react';
+import { shallow } from 'enzyme';
+
+import { findByTestAttr } from '../testUtils';
 import App from '../../components/App';
 
+const setup = (state=null) => {
+  const wrapper = shallow(<App />);
+  if (state) wrapper.setState(state);
+  return wrapper;
+};
+
+describe('renders properly', () => {
+  test('renders without crashing', () => {
+    const wrapper = setup();
+    const app = findByTestAttr(wrapper, 'App');
+    expect(app.length).toBe(1);
+  });
+  test('renders Loading component while info is being fetched', () => {
+    const wrapper = setup({ taxis: null, cars: null });
+    const loading = findByTestAttr(wrapper, 'loading');
+    expect(loading.length).toBe(1);
+  });
+  test('unmounts Loading component and renders container when info has been fetched', () => {
+    const wrapper = setup({ taxis: [{}, {}], cars: [{}, {}] });
+    const loading = findByTestAttr(wrapper, 'loading');
+    const container = findByTestAttr(wrapper, 'container');
+    expect(loading.length).toBe(0);
+    expect(container.length).toBe(1);
+  });
+});
